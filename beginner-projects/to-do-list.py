26185115e3839876e80fd022bc06943d852cbc0f
Kathy <kathylam2001@gmail.com>
2025-11-14 19:14:54 -0500

    Update to-do-list.py

--- beginner-projects/to-do-list.py
+++ beginner-projects/to-do-list.py
@@ -29,10 +29,10 @@ def main():
 
     while True:
         print("\n~To-Do List~")
-        print("1. Add Task")
-        print("2. Show Tasks")
-        print("3. Mark Task as Done")
-        print("4. Exit")
+        print("1) Add Task")
+        print("2) Show Tasks")
+        print("3) Mark Task as Done")
+        print("4) Exit")
 
         choice = input("Enter your choice: ")
 
